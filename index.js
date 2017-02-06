7cda7d3f5a30b35e798d6c68c9b96074ea552b43
Brandon Semilla <semibran@gmail.com>
2017-02-05 23:07:39 -0800

    Add lib

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+	// Top (-1)
+	TOP_LEFT:  [-1, -1],
+	TOP:       [ 0, -1],
+	TOP_RIGHT: [ 1, -1],
+	// Center (0)
+	LEFT:  [-1, 0],
+	RIGHT: [ 1, 0],
+	// Bottom (1)
+	BOTTOM_LEFT:  [-1, 1],
+	BOTTOM:       [ 0, 1],
+	BOTTOM_RIGHT: [ 1, 1]
+}
